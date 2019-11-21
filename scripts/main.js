f2b9d285d480be1ccaa15217b304fde178fd616d
Tyler Bisig <bisig1892@gmail.com>
2019-11-21 14:41:34 -0800

    added a display function from api. basic html template

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,54 @@
 window.onload = function () {
-    var memeBtn = document.getElementById('get-meme');
-    memeBtn.onclick = fetchMeme;
+    var jokeBtn = document.getElementById('get-joke');
+    jokeBtn.onclick = fetchJoke;
+    populateCategories();
 };
-function fetchMeme() {
+function fetchJoke() {
+    var jokeBtn = this;
+    jokeBtn.disabled = true;
+    var loaderImg = document.getElementById('loader');
+    loaderImg.classList.add('loader');
     var request = new XMLHttpRequest();
-    request.onreadystatechange = handleMemeResponse;
+    request.onreadystatechange = handleJokeResponse;
     request.open('GET', 'https://api.icndb.com/jokes/random');
     request.send();
 }
-function handleMemeResponse() {
+function handleJokeResponse() {
     var request = this;
+}
+function displayJoke(j) {
+    var jokeTextPar = document.getElementById('joke-text');
+    jokeTextPar.innerHTML = j.joke;
+    var jokeIdPar = document.getElementById('joke-id');
+    jokeIdPar.innerHTML = 'Id: ' + j.id.toString();
+    var categoryList = document.getElementById('categories');
+    categoryList.innerHTML = '';
+    for (var i = 0; i < j.categories.length; i++) {
+        var item = document.createElement('li');
+        item.innerHTML = j.categories[i];
+        categoryList.appendChild(item);
+    }
+    var catDisplay = document.getElementById('category-display');
+    if (j.categories.length == 0) {
+        catDisplay.style.display = 'none';
+    }
+    else {
+        catDisplay.style.display = 'block';
+    }
+    var loaderImg = document.getElementById('loader');
+    loaderImg.classList.remove('loader');
+    setTimeout(function () {
+        var jokeBtn = document.getElementById('get-joke');
+        jokeBtn.disabled = false;
+    }, 3000);
+}
+function populateCategories() {
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://api.icndb.com/categories');
+    request.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var categories = JSON.parse(this.responseText).value;
+        }
+    };
+    request.send();
 }
--- typescript/main.ts
+++ typescript/main.ts
@@ -1,12 +1,18 @@
 window.onload = function() {
-    let memeBtn = document.getElementById('get-meme');
-    memeBtn.onclick = fetchMeme;
+    let jokeBtn = document.getElementById('get-joke');
+    jokeBtn.onclick = fetchJoke;
+
+    populateCategories();
 }
 
-function fetchMeme() {
-    let request = new XMLHttpRequest();
-    request.onreadystatechange = handleMemeResponse;
+function fetchJoke() {
+    let jokeBtn = <HTMLButtonElement>this;
+    jokeBtn.disabled = true;
+    let loaderImg = document.getElementById('loader');
+    loaderImg.classList.add('loader');
 
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = handleJokeResponse;
 
     // Set URL to send request to:
     request.open('GET', 'https://api.icndb.com/jokes/random');
@@ -14,9 +20,57 @@ function fetchMeme() {
     request.send();
 }
 
-function handleMemeResponse() {
+function handleJokeResponse() {
     let request = <XMLHttpRequest>this;
 
     //readyState 4 means reuest is finished
     //status 200 means "OK" - success
+}
+
+function displayJoke(j:ChuckNorrisJoke):void {
+    let jokeTextPar = document.getElementById('joke-text');
+    jokeTextPar.innerHTML = j.joke;
+    
+    let jokeIdPar = document.getElementById('joke-id');
+    jokeIdPar.innerHTML = 'Id: ' + j.id.toString();
+
+    let categoryList = document.getElementById('categories');
+    // Clear out categories from any previous joke
+    categoryList.innerHTML = '';
+
+    for(let i = 0; i < j.categories.length; i++) {
+        let item = document.createElement('li');
+        item.innerHTML = j.categories[i];
+        categoryList.appendChild(item);
+
+    }
+    let catDisplay = document.getElementById('category-display');
+    if(j.categories.length == 0) {
+        catDisplay.style.display = 'none';
+    } else {
+        catDisplay.style.display = 'block';
+    }
+
+    let loaderImg = document.getElementById('loader');
+    loaderImg.classList.remove('loader');
+
+    //re-enabling joke button 3 seconds after joke has loaded
+    setTimeout(function() {
+        let jokeBtn = <HTMLButtonElement>document.getElementById('get-joke');
+        jokeBtn.disabled = false;
+    }, 3000)
+}
+
+//Display categories in a dropdown list
+
+function populateCategories() {
+    let request = new XMLHttpRequest();
+    request.open('GET', 'https://api.icndb.com/categories')
+
+    request.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            let categories:string[] = JSON.parse(this.responseText).value;
+        }
+    }
+    request.send();
 }
